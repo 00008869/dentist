acd79f6d3e3f7e8ed6577505a26879540396e3e3
00008869 <73437737+00008869@users.noreply.github.com>
2022-03-28 23:22:25 +0500

    New version

---
+++ telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+
+$phone = $_POST['user_phone'];
+
+$token = "5190563084:AAFHSOlowsrPXmk7hcHDOIJ5mopjd8Didig";
+$chat_id = "-679966720";
+$arr = array(
+
+  'Телефон: ' => $phone
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
